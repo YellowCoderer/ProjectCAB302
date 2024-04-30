f201bc0aab2ac4b3a5b47ea887f1fbcbf56a62b6
woahhoaw <danieltwaterson@outlook.com>
2024-04-30 19:19:32 +1000

    Add some function the Timer, Statistics, and Home Button

--- src/main/java/com/example/sleepwell/HelloController.java
+++ src/main/java/com/example/sleepwell/HelloController.java
@@ -4,6 +4,7 @@ import javafx.animation.TranslateTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
@@ -27,6 +28,9 @@ public class HelloController implements Initializable {
 
     @FXML
     private AnchorPane LeftSlider, RightSlider;
+
+    @FXML
+    private Button  StatisticsButton, HomeButton, TimerButton;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -80,7 +84,23 @@ public class HelloController implements Initializable {
             slide.setOnFinished((ActionEvent e) -> {
                 Profile.setVisible(true);
                 ProfileClose.setVisible(false);
+
             });
+        });
+        StatisticsButton.setOnMouseClicked(event -> {   //close the menu on button click
+            TranslateTransition slide = new TranslateTransition(Duration.seconds(0.4), LeftSlider);
+            slide.setToX(-123);
+            slide.play();
+        });
+        TimerButton.setOnMouseClicked(event -> {    //close the menu on button click
+            TranslateTransition slide = new TranslateTransition(Duration.seconds(0.4), LeftSlider);
+            slide.setToX(-123);
+            slide.play();
+        });
+        HomeButton.setOnMouseClicked(event -> {     //close the menu on button click
+            TranslateTransition slide = new TranslateTransition(Duration.seconds(0.4), LeftSlider);
+            slide.setToX(-123);
+            slide.play();
         });
     }
 }
