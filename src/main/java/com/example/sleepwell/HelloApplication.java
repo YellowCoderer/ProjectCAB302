f49982ced87bd1c25a2d21f5a83b2546c2add69f
ai10011 <rodchuephollapat1@gmail.com>
2024-04-30 14:35:14 +1000

    update signup

--- src/main/java/com/example/sleepwell/HelloApplication.java
+++ src/main/java/com/example/sleepwell/HelloApplication.java
@@ -12,9 +12,11 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
+//        Parent root = FXMLLoader.load(getClass().getResource("hello-view.fxml")); // Home
+        Parent root = FXMLLoader.load(getClass().getResource("signup.fxml")); // SignUp test
         String stylesheet = HelloApplication.class.getResource("stylesheet.css").toExternalForm();
-        Scene scene = new Scene(root, 600, 400);
+//        Scene scene = new Scene(root, 600, 400); // Home
+        Scene scene = new Scene(root, 520, 567); // SignUp test
         scene.getStylesheets().add(stylesheet);
         stage.setScene(scene);
         stage.show();
--- src/main/java/com/example/sleepwell/SignupController.java
+++ src/main/java/com/example/sleepwell/SignupController.java
@@ -1,51 +1,75 @@
 package com.example.sleepwell;
 
+import com.example.sleepwell.database.Accounts;
+import com.example.sleepwell.database.SqliteAccountDAO;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
-import javafx.scene.control.*;
+import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.control.PasswordField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+
 
 import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.sql.Connection;
 import java.util.ResourceBundle;
+
 
 public class SignupController implements Initializable {
 
     @FXML
-    private Hyperlink loginLink;
-    @FXML
     private Label signupMessageLabel;
+    @FXML
+    private Button closeButton;
     @FXML
     private PasswordField setPasswordField;
     @FXML
     private PasswordField confirmPasswordField;
+    @FXML
+    private ImageView sleepImageView;
+    @FXML
+    private Label confirmPasswordLabel;
 
-    public void signupButtonOnAction(ActionEvent event){
-        signupMessageLabel.setText("User has been signup successfully");
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        File sleepFile = new File("src/main/resources/images/enough-sleep.png");
+        Image sleepImage = new Image(sleepFile.toURI().toString());
+        sleepImageView.setImage(sleepImage);
     }
-//    public void loginLink(ActionEvent event) throws Exception {
-//        FXMLLoader loader = new FXMLLoader(getClass().getResource("login.fxml"));
-//        Parent root = loader.load();
-//
-//        Scene scene = new Scene(root);
-//
-//        Stage stage = (Stage) loginLink.getScene().getWindow();
-//
-//        stage.setScene(scene);
-//        stage.show;
-//    }
-
-    public void registerUser(){
+    public void signupButtonOnAction(ActionEvent event){
 
         if (setPasswordField.getText().equals(confirmPasswordField.getText())) {
-
+//            registerUser();
+            confirmPasswordLabel.setText("");
+            signupMessageLabel.setText("User has been signup successfully");
         }
         else {
-
+            confirmPasswordLabel.setText("Password does not match");
         }
     }
+
+    public void closeButtonOnAction(ActionEvent event){
+        Stage stage = (Stage) closeButton.getScene().getWindow();
+        stage.close();
+        Platform.exit();
+    }
+
+//    public void registerUser(){
+//        DatabaseConnection connectNow = new DatabaseConnection();
+//        Connection connectDB = connectNow.getConnection();
+//
+//        String firstname = "";
+//        String lastname = "";
+//        String username = "";
+//
+//    }
 }
