10810ed80782c34ae2624d88a34f096530f5d196
Arjyu1 <arjunrnair21012004@gmail.com>
2024-05-13 16:53:44 +1000

    basic stopwatch

--- src/main/java/com/example/sleepwell/controller/TimerController.java
+++ src/main/java/com/example/sleepwell/controller/TimerController.java
@@ -1,4 +1,38 @@
 package com.example.sleepwell.controller;
 
-public class TimerController {
+import javafx.animation.AnimationTimer;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class TimerController extends HelloController {
+    @FXML
+    private Label stopwatchLabel;
+
+    private long startTime;
+
+    private AnimationTimer timer = new AnimationTimer() {
+        @Override
+        public void handle(long now) {
+            long elapsedMillis = (System.nanoTime() - startTime) / 1_000_000;
+            long seconds = (elapsedMillis / 1000) % 60;
+            long minutes = (elapsedMillis / (1000 * 60)) % 60;
+            long hours = (elapsedMillis / (1000 * 60 * 60)) % 24;
+            stopwatchLabel.setText(String.format("%02d:%02d:%02d", hours, minutes, seconds));
+        }
+    };
+
+    public void startStopwatch() {
+        startTime = System.nanoTime();
+        timer.start();
+    }
+
+    public void stopStopwatch() {
+        timer.stop();
+    }
+
+    public void resetStopwatch() {
+        timer.stop();
+        stopwatchLabel.setText("00:00:00");
+    }
 }
+
