bba448621d8733c5cfb87319167d3e12124019aa
YellowCoderer <dave66131@hotmail.com>
2024-05-25 16:25:41 +1000

    Fix the update sleepschedule

--- src/main/java/com/example/sleepwell/timer/SqliteSleepScheduleDAO.java
+++ src/main/java/com/example/sleepwell/timer/SqliteSleepScheduleDAO.java
@@ -49,33 +49,42 @@ public class SqliteSleepScheduleDAO implements ISleepScheduleDAO{
         }
     }
 
-//    @Override
-//    public void updateSleepSchedule(SleepSchedule sleepschedule) {
-//        try {
-//            PreparedStatement statement = connection.prepareStatement("UPDATE schedule SET monday = ?, tuesday = ?, wednesday = ?, thursday = ?, friday = ?, saturday = ?, sunday = ?, WHERE userid = ?");
-//            statement.setInt(6, sleepschedule.getSleepScheduleId());
-//            statement.setString(1, sleepschedule.getMonday());
-//            statement.setString(2, sleepschedule.getTuesday());
-//            statement.setString(3, sleepschedule.getWednesday());
-//            statement.setString(4, sleepschedule.getThursday());
-//            statement.setString(5, sleepschedule.getFriday());
-//            statement.setString(5, sleepschedule.getSaturday());
-//            statement.setString(5, sleepschedule.getSunday());
-//
-//            statement.executeUpdate();
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//    }
-@Override
+    @Override
     public void updateSleepSchedule(SleepSchedule sleepschedule) {
         try {
-            PreparedStatement statement = connection.prepareStatement("UPDATE schedule SET monday = '"+sleepschedule.getMonday()+"', tuesday = '"+sleepschedule.getTuesday()+"', wednesday = '"+sleepschedule.getWednesday()+"', thursday = '"+sleepschedule.getThursday()+"', friday = '"+sleepschedule.getFriday()+"', saturday = '"+sleepschedule.getSaturday()+"', sunday = '"+sleepschedule.getSunday()+"' WHERE userid = "+sleepschedule.getSleepScheduleId());
+            PreparedStatement statement = connection.prepareStatement("UPDATE schedule SET monday = ?, tuesday = ?, wednesday = ?, thursday = ?, friday = ?, saturday = ?, sunday = ? WHERE userid = ?");
+            statement.setString(1, sleepschedule.getMonday());
+            statement.setString(2, sleepschedule.getTuesday());
+            statement.setString(3, sleepschedule.getWednesday());
+            statement.setString(4, sleepschedule.getThursday());
+            statement.setString(5, sleepschedule.getFriday());
+            statement.setString(6, sleepschedule.getSaturday());
+            statement.setString(7, sleepschedule.getSunday());
+            statement.setInt(8, sleepschedule.getSleepScheduleId());
+
             statement.executeUpdate();
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
+//    @Override
+//    public void updateSleepSchedule(SleepSchedule sleepschedule) {
+//        try {
+//            Statement statement = connection.createStatement();
+//            String insertQuery = "UPDATE schedule " +
+//                    "SET monday = '"+sleepschedule.getMonday()+"', " +
+//                    "tuesday = '"+sleepschedule.getTuesday()+"', " +
+//                    "wednesday = '"+sleepschedule.getWednesday()+"', " +
+//                    "thursday = '"+sleepschedule.getThursday()+"', " +
+//                    "friday = '"+sleepschedule.getFriday()+"', " +
+//                    "saturday = '"+sleepschedule.getSaturday()+"', " +
+//                    "sunday = '"+sleepschedule.getSunday()+"' " +
+//                    "WHERE userid = "+sleepschedule.getSleepScheduleId()+";";
+//            statement.executeUpdate(insertQuery);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
 
     @Override
     public void deleteSleepSchedule(SleepSchedule sleepschedule) {
